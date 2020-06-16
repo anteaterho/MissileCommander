0a16f6b1d9895824d32b271ec52b91a0d5b19bd4
Seungho Choi <anteaterho@gmail.com>
2020-06-16 19:28:31 +0900

    74page

---
+++ MissileCommanderProject/Assets/_Scripts/BulletLauncher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLauncher : MonoBehaviour
+{
+    IGameController controller;
+
+    public void SetGameController(IGameController controller)
+    {
+        this.controller = controller;
+    }
+
+    private void Update()
+    {
+     
+        if(controller != null)
+        {
+            if (controller.FirebuttonPressed())
+            {
+                Debug.Log("Fired a bullet!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Controller is null!");
+        }
+    }
+
+}
---
+++ MissileCommanderProject/Assets/_Scripts/GameManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //[SerializeField]
+    BulletLauncher launcherPrefab;
+    BulletLauncher launcher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launcher = Instantiate(launcherPrefab);
+        launcher.SetGameController(new MouseGameController());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
